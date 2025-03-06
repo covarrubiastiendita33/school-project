039621af6e609b8fc33f1c986953fa158fd18762
covarrubiastiendita33 <covarrubiastiendita33@gmail.com>
2025-03-06 13:32:36 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int x = 4;
+    int y = 5;
+    int z = x + y;
+    std::cout << "The sum of " << x << " and " << y << " is: " << z << std::endl;
+    return 0;
+}
