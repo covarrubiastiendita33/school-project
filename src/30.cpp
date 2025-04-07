8cb5ba1597a4c6e243dcd3968971d8a277660909
covarrubiastiendita33 <covarrubiastiendita33@gmail.com>
2025-04-07 05:28:08 +0000

    Add 30.cpp

---
+++ src/30.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "X is positive." << endl;
+    } else {
+        cout << "X is not positive." << endl;
+    }
+    
+    return 0;
+}
