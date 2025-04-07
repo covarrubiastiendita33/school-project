4818275b2cd4515be923a4c5bf692ee4c84c95de
covarrubiastiendita33 <covarrubiastiendita33@gmail.com>
2025-04-07 05:14:17 +0000

    Add 29.cpp

---
+++ src/29.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        std::cout << "Num1 is greater than Num2." << std::endl;
+    } else {
+        std::cout << "Num1 is not greater than Num2." << std::endl;
+    }
+
+    return 0;
+}
