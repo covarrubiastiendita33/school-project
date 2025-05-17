9877b5b76804be8f4cf52b424c5c86479b2e9bde
covarrubiastiendita33 <covarrubiastiendita33@gmail.com>
2025-05-17 08:07:38 +0000

    Add 44.cpp

---
+++ src/44.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age = 18;
+    std::cout << "Hello, world!" << std::endl;
+
+    if (age >= 18) {
+        std::cout << "You are an adult." << std::endl;
+    } else {
+        std::cout << "You are a minor." << std::endl;
+    }
+
+    return 0;
+}
